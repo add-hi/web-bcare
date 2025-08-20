dda1627107d0b4dd503d5d121ed22046e86e99de
Muhammad Zidan Ramdhani <ramdhani2002@gmail.com>
2025-08-20 11:18:20 +0700

    refactor: clean up on AddComplaint

--- src/components/AddComplaint.js
+++ src/components/AddComplaint.js
@@ -4,15 +4,9 @@ import CustomerForm from "@/components/form/CustomerForm";
 import DataForm from "@/components/form/DataForm";
 import ActionForm from "@/components/form/ActionForm";
 import NotesForm from "@/components/form/NotesForm";
-import FloatingCustomerContact from "@/components/FloatingCustomerContact";
 import InputFormRow from "@/components/form/InputFormRow";
-import { ArrowLeft } from "lucide-react";
 
 function AddComplaint() {
-  const handleBack = () => {
-    window.history.back(); // kembali ke halaman sebelumnya
-  };
-
   return (
     <div>
       <InputFormRow />
@@ -20,7 +14,6 @@ function AddComplaint() {
       <DataForm />
       <NotesForm />
       <ActionForm />
-      {/* <FloatingCustomerContact /> */}
     </div>
   );
 }
